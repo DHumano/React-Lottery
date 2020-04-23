7d40c71e2ae9c6fe2ab8f0c1ff1694d871ed19f6
dario <darioh.dev@gmail.com>
2020-04-23 14:05:24 -0300

    Updates

---
+++ precache-manifest.a11f5adad58d74d4f9faf29d99278a8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3bb4b9e23e62077c6526bcfa72409b",
+    "url": "/React-Lottery/index.html"
+  },
+  {
+    "revision": "19e87e48289c59c430e6",
+    "url": "/React-Lottery/static/css/main.2ceca0eb.chunk.css"
+  },
+  {
+    "revision": "13597667145840357232",
+    "url": "/React-Lottery/static/js/2.b54fb3e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Lottery/static/js/2.b54fb3e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19e87e48289c59c430e6",
+    "url": "/React-Lottery/static/js/main.09ffa1eb.chunk.js"
+  },
+  {
+    "revision": "a464e176cc72faab4ade",
+    "url": "/React-Lottery/static/js/runtime-main.06ac39a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Lottery/static/media/logo.5d5d9eef.svg"
+  }
+]);
